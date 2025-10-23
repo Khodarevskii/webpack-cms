53c37325b506791134c7bf01cdbfa13ac455f41c
kirill <khodarevskii@yandex.ru>
2025-10-24 02:25:27 +0300

    довел сборку до ума

---
+++ src/scripts/content.js
@@ -0,0 +1,2 @@
+import '../scss/index.scss';
+const images = require.context('../images', false, /\.(png|jpe?g|gif|svg|webp)$/);
--- webpack.config.js
+++ webpack.config.js
@@ -2,61 +2,138 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-   optimization: {
-   minimize: false
-},
-   mode: "development",
-  entry:`./src/scripts/index.js`,
+  mode: "development",
+  entry: {
+    main: './src/scripts/index.js',       
+    'content': './src/scripts/content.js',
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: 'js/[name].js',
     publicPath: '',
   },
-  mode: 'development',
+  devtool: false,
   devServer: {
     static: path.resolve(__dirname, './dist'),
     open: true,
     port: 8080
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            compact: false,
+            minified: false,
+            comments: false,
+          }
+        },
+        exclude: /node_modules/
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
       },
       {
-          test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-
-                    "css-loader",
-                     
-
-                  'postcss-loader',
-
-                    "sass-loader",
-                    
-                ],
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          'postcss-loader',
+          "sass-loader",
+        ],
       },
-      
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      },
     ],
-   
   },
-  
+  optimization: {
+    minimize: true,
+    
+    runtimeChunk: 'single', 
+    
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          priority: 20,
+          enforce: true,
+        },
+        common: {
+          minChunks: 2,
+          name: 'common',
+          priority: 10,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: false,       
+          mangle: false,         
+          keep_classnames: true,
+          keep_fnames: true,
+          format: {
+            comments: false,    
+            beautify: true,
+            indent_level: 2,
+            preserve_annotations: true,
+          },
+        },
+        extractComments: false,  
+      }),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['mozjpeg', { quality: 85, progressive: true }],
+              ['pngquant', { quality: [0.75, 0.90], speed: 1 }],
+              ['optipng', { optimizationLevel: 7 }],
+              ['gifsicle', { interlaced: true, optimizationLevel: 3 }],
+              ['svgo', {
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: { removeViewBox: false },
+                    },
+                  },
+                ],
+              }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
       minify: false
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
   ],
- 
-}
+};
